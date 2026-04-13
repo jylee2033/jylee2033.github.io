22574321ffb394d5ddb635b235e6f4939a26ef30
jla670 <jla670@sfu.ca>
2026-04-12 21:12:34 -0700

    add redirects from old Jekyll post URLs to Astro project URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://jylee2033.github.io',
   output: 'static',
+  redirects: {
+    '/posts/software-test-engineer/': '/projects/software-test-engineer/',
+    '/posts/automation-tester/': '/projects/automation-tester/',
+  },
 });
