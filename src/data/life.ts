2892aae1d7e94715cabbeb22c86aa43558a0c599
jla670 <jla670@sfu.ca>
2026-04-16 21:35:44 -0700

    update piano video list

--- src/data/life.ts
+++ src/data/life.ts
@@ -9,10 +9,11 @@ export interface Photo {
 }
 
 export const pianoVideos: Video[] = [
-  { id: "nGdU0ipazQA", title: "Suzume OST" },
+  { id: "JS9NuP3pAIo", title: "My Neighbor Totoro" },
   { id: "-xm4QMR2E9c", title: "Pirates of the Caribbean" },
   { id: "QIpcUm9j9tg", title: "Super Mario" },
-  { id: "hQoicvqabnY", title: "Mikrokosmos 소우주" },
+  { id: "EGBz0ZjGC_Q", title: "Ponyo On The Cliff" },
+  { id: "nGdU0ipazQA", title: "Suzume OST" },
 ];
 
 export const bakingPhotos: Photo[] = [];
