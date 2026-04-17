acb7a6ab1e5efcdd81f941f7b8b37c2f826e4c36
jla670 <jla670@sfu.ca>
2026-04-16 21:54:53 -0700

    add baking photos to life section

--- src/data/life.ts
+++ src/data/life.ts
@@ -16,6 +16,27 @@ export const pianoVideos: Video[] = [
   { id: "nGdU0ipazQA", title: "Suzume OST" },
 ];
 
-export const bakingPhotos: Photo[] = [];
+export const bakingPhotos: Photo[] = [
+  { src: "/life/baking/0A15A308-DE85-45B7-A98F-72D226C34D8B.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_0419.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_0554.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_1316.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_2034.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_2530.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_2879.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_2916.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_4909.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_4917.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_6327.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_6494.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_6966.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_6969.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_6973.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_9045.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_9221.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_9370.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_9461.jpg", alt: "Baking" },
+  { src: "/life/baking/IMG_9492.jpg", alt: "Baking" },
+];
 
 export const photographyPhotos: Photo[] = [];
